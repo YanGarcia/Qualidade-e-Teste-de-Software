6b25b05c85fa6a13f4c676e140f0a561afdb68de
YanGarcia <65686063+YanGarcia@users.noreply.github.com>
2025-03-14 21:32:56 -0300

    Código Java verificado e ajustado
    De acordo com o plugin CheckStyle, tipo de verificação: Sun Checks.

---
+++ Calculadora.java
@@ -0,0 +1,42 @@
+public class Calculadora {
+    /**
+     * Soma dois valores.
+     * @param valor1
+     * @param valor2
+     * @return valor1 + valor2
+     */
+    public double somar(final double valor1, final double valor2) {
+        return valor1 + valor2;
+    }
+
+    /**
+     * Subtrai dois .
+     * @param valor1
+     * @param valor2
+     * @return valor1 - valor2
+     */
+    public double subtrair(final double valor1, final double valor2) {
+        return valor1 - valor2;
+    }
+
+    /**
+     * Multiplica dois valores.
+     * @param valor1
+     * @param valor2
+     * @return valor1 * valor 2
+     */
+    public double multiplicar(final double valor1, final double valor2) {
+        return valor1 * valor2;
+    }
+
+    /**
+     * Divide dois valores.
+     * @param valor1
+     * @param valor2
+     * @return valor1 / valor2
+     */
+    public double dividir(final double valor1, final double valor2) {
+        return valor1 / valor2;
+    }
+
+}
